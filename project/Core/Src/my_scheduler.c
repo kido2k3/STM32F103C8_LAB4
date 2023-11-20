019c630bff9fa6b7c1b586e1cc97bcd2fa5a3608
Kido <hoangphongvip2003@gmail.com>
2023-11-20 21:22:54 +0700

    add more comments

--- project/Core/Src/my_scheduler.c
+++ project/Core/Src/my_scheduler.c
@@ -7,6 +7,12 @@
 
 #include "my_scheduler.h"
 
+/*
+@brief: pTask - pointer pointing to the function that will run
+		counter - the amount of remaining time or the time after the previous task
+		period - the amount of cycle time to run task
+		next_task - pointer pointing to the next task
+*/
 struct task
 {
     void (*pTask)();
@@ -14,6 +20,9 @@ struct task
     uint16_t period;
     struct task *next_task;
 };
+/*
+@brief: top - the top of task stack
+*/
 struct
 {
     struct task *top;
@@ -21,6 +30,14 @@ struct
     // uint32_t time_length;
 } stack_task;
 // struct task *dispatcher;
+/*
+ * @brief:	add a new task to the task stack
+ * @para:	pTask - pointer pointing to the function that will run
+			delay - the amount of time after which the task will run
+			period - the amount of cycle time to run task
+ * @retval:	1 - add successfully
+ * 			0 - add badly
+ * */
 bool sch_add_task(void (*pTask)(), uint16_t delay, uint16_t period)
 {
     struct task *my_task = (struct task *)malloc(sizeof(struct task));
@@ -65,6 +82,11 @@ bool sch_add_task(void (*pTask)(), uint16_t delay, uint16_t period)
         cur->counter -= my_task->counter;
     return 1;
 }
+/*
+ * @brief:	decrease the remaining time of the top task
+ * @para:	none
+ * @retval:	none
+ * */
 void sch_update(void)
 {
     if (stack_task.top == 0)
@@ -74,6 +96,11 @@ void sch_update(void)
         stack_task.top->counter--;
     }
 }
+/*
+ * @brief:	delete a task in the stack and release the memory
+ * @para:	del_task - the task need to be deleted
+ * @retval:	none
+ * */
 void sch_delete_task(struct task *del_task)
 {
     if (del_task == 0)
@@ -82,7 +109,12 @@ void sch_delete_task(struct task *del_task)
     }
     free(del_task);
 }
-
+/*
+ * @brief:	run the top task and update if over remainning time
+ * @para:	none
+ * @retval:	1 - run succesfully
+ * 			0 - run badly
+ * */
 bool sch_dispatch(void)
 {
     if (stack_task.top == 0)
