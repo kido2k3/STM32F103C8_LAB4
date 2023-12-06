c81e40eaed517f78f537bad556c21707c629bba2
Kido <hoangphongvip2003@gmail.com>
2023-12-06 19:54:07 +0700

    add rm task

--- project/Core/Inc/my_scheduler.h
+++ project/Core/Inc/my_scheduler.h
@@ -1,17 +1,23 @@
 /*
  * scheduler.h
  *
- *  Created on: Nov 18, 2023
+ *  Created on: Nov 29, 2023
  *      Author: HP
  */
 
 #ifndef INC_MY_SCHEDULER_H_
 #define INC_MY_SCHEDULER_H_
 
+#include <my_keypadTTP229.h>
 #include"my_define.h"
+#include "my_lcd.h"
 #include<stdlib.h>
 
-bool sch_add_task(void (*pTask)(), uint16_t delay, uint16_t period);
+#define ONE_SECOND	1000
+
 void sch_update(void);
+void sch_init(void);
 bool sch_dispatch(void);
+bool sch_add_task(void (*pTask)(), uint16_t delay, uint16_t period);
+void sch_remove_task(void (*pTask)());
 #endif /* INC_MY_SCHEDULER_H_ */
